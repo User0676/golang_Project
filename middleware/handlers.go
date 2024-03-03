dfce5a3e9027c154cf865c8ca21c5a9883442040
Ali Zhumatayev <alizhumataeyev@Alis-MacBook-Air-2.local>
2024-03-04 00:28:40 +0500

    still in process

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,6 +1,76 @@
 package middleware
 
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"golang_Project/models"
+	"log"
+	"net/http"
+	"os"
+)
+
 type response struct {
 	ID      int64  `json:"id,omitempty"`
 	Message string `json:"message,omitempty"`
+}
+
+func CreateConnection() *sql.DB {
+	err := godotenv.Load(".env")
+
+	if err != nill {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+
+	if err != nill {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nill {
+		panic(err)
+	}
+
+	fmt.Println("Succesfully connected to postgres")
+	return db
+}
+
+func GetInstructor() {
+
+}
+
+func GetAllInstructor() {
+
+}
+
+func HireInstructor(w http.ResponseWriter, r *http.Request) {
+	var instructor models.Instructor
+
+	err := json.NewDecoder(r.Body).Decode(&instructor)
+
+	if err != nill {
+		log.Fatal("Unable to decode the request body. %v", err)
+	}
+
+	insertID := insertInstructor(instructor)
+
+	res := response{
+		ID:      insertID,
+		Message: "instructor hired succesfully",
+	}
+
+	json.NewDecoder(w).Encode(res)
+
+}
+
+func UpdateInstructor() {
+
+}
+
+func FireInstructor() {
+
 }
